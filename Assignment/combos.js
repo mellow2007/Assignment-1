8bea56fdff6e517ba8503995eeca33dffe563a80
mellow2007 <S10267904@connect.np.edu.sg>
2024-12-01 20:24:10 +0800

    Delete Assignment directory

--- Assignment/combos.js
+++
@@ -1,8 +0,0 @@
-function toggleComboDetails(comboId) {
-    const combo = document.getElementById(comboId);
-    if (combo.style.display === 'block') {
-        combo.style.display = 'none';
-    } else {
-        combo.style.display = 'block';
-    }
-}
